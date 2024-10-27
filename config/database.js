9920f0916041004cb8c6b0ff842da5415fe5483f
Linda BOUGAA <lindabgaa@Host-003.lan>
2024-10-27 20:38:05 +0100

    fixed bugs #2

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@ const config = require("../config/config.json");
 const env = process.env.NODE_ENV || "development";
 const dbConfig = config[env];
 
+console.log("dbConfig", dbConfig);
+
 const sequelize = new Sequelize({
   dialect: dbConfig.dialect,
   storage: dbConfig.storage,
